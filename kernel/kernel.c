84a7c10b47e48eca40f1095693484ccf72504076
crazyxy <xueyansjtu@sjtu.edu.cn>
2017-08-01 17:25:38 +0800

    add paging

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -18,7 +18,7 @@ void user_input(char *input){
         asm volatile("hlt");
     }else if(strcmp(input, "PAGE") == 0){
         uint32_t phys_addr;
-        uint32_t page = kmalloc(1000, 1, &phys_addr);
+        uint32_t page = kmalloc_ap(1000, &phys_addr);
         char page_str[16] = "";
         hex_to_ascii(page, page_str);
         char phys_str[16] = "";
--- kernel/paging.h
+++ kernel/paging.h
@@ -1,6 +1,7 @@
 #ifndef PAGING_H
 #define PAGING_H
 
+#include <cpu/isr.h>
 
 typedef struct page {
     uint32_t present    : 1;
@@ -39,8 +40,8 @@ uint32_t nframes;
 
 extern uint32_t placement_address;
 
-#define INDEX_FROM_BIT(a) (a/32)
-#define OFFSET_FROM_BIT(a) (a%32)
+#define INDEX_FROM_BIT(a) (a>>5)
+#define OFFSET_FROM_BIT(a) (a&31)
 
 static void set_frame(uint32_t frame_addr){
     uint32_t frame = frame_addr>>12;
@@ -53,7 +54,48 @@ static void clear_frame(uint32_t frame_addr){
     uint32_t frame = frame_addr>>12;
     uint32_t idx = INDEX_FROM_BIT(frame);
     uint32_t off = OFFSET_FROM_BIT(frame);
-    frames[idx]
+    frames[idx] &= (~(0x1 << off));
+}
+
+static uint32_t first_frame(){
+	uint32_t i, j;
+	for(i = 0; i < INDEX_FROM_BIT(nframes); i++){
+		if(frames[i] != 0xffffffff){
+			for(j = 0; j < 32; j++){
+				if(!(frames[i]&(0x1<<j))){
+					return i*32+j;
+				}
+			}
+		}
+	}
+	return -1;
+}
+
+void alloc_frame(page_t *page, int is_kernel, int is_writeable){
+	if(page->frame){
+		return;
+	}else{
+		uint32_t idx = first_frame();
+		if(idx == (uint32_t)-1){
+			return;
+		}
+		set_frame(idx*0x1000);
+		page->present = 1;
+		page->rw = is_writeable ? 1 : 0;
+		page->user = is_kernel ? 0 : 1;
+		page->frame = idx;
+	}
+}
+
+
+void free_frame(page_t *page){
+	uint32_t frame;
+	if(!(frame=page->frame)){
+		return;
+	}else{
+		clear_frame(frame);
+		page->frame = 0x0;
+	}
 }
 
 #endif
